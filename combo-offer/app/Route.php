7deb298d6ad098800de62ee8960266508248fc51
YEGAPPAN-S <85267861+YEGAPPAN-S@users.noreply.github.com>
2022-09-16 12:59:45 +0530

    Add files via upload

--- combo-offer/app/Route.php
+++ combo-offer/app/Route.php
@@ -1,16 +1,44 @@
 <?php 
-//Product data tabs 
-add_filter( 'woocommerce_product_data_tabs' , 'combo_tab' );
+/**
+ * Combo Offer
+ *
+ * @package   combo-offer
+ * @author    Yegappan
+ * @copyright 2022 Flycart
+ * @license   GPL v2 or later
+ * @link      https://flycart.org
+ */
 
-//Product data panel
-add_action( 'woocommerce_product_data_panels' , 'combo_tab_data' );
+namespace Combo\App;
 
-//Save meta post
-add_action( 'save_post' , 'combo_save_post' );
+defined( 'ABSPATH' ) || exit;
 
-//Product view page
-add_action( 'woocommerce_after_single_product' , 'combo_after_product' );
+class Route
+{
+    /**
+     * Plugin hooks
+     */
+    public static function hooks()
+    {
 
-//add to cart 
-add_action( 'wp' , 'combo_product_add_to_cart' );
+            $control= new Controllers\Admin\Controller();
+
+            //Product data tabs
+            add_filter('woocommerce_product_data_tabs',[$control, 'combo_tab']);
+
+            //Product data panel
+            add_action('woocommerce_product_data_panels',[$control, 'combo_tab_data']);
+
+            
+            //Save meta post
+            add_action('save_post',[$control, 'combo_save_post']);
+
+            //Add to cart
+            add_action('wp',[$control, 'combo_product_add_to_cart']);
+            
+            //Product view page
+            add_action('woocommerce_after_single_product',[$control, 'combo_after_product']);
+  
+    }
+}
 ?>
--- combo-offer/combo-offer.php
+++ combo-offer/combo-offer.php
@@ -1,8 +1,8 @@
 <?php
 /*
-  Plugin Name:      Combo
+  Plugin Name:      Combo Offer
   Plugin URI:       http://wordpress.org
-  Description:      Combo Offers on woocommerce plugin
+  Description:      Combo Offers for Woocommerce Plugin
   Author:           Yegappan
   Author URI:       http://cartrabbit.io/
   Text Domains:     combo
@@ -13,54 +13,11 @@
   License URI:      http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
+
 defined( 'ABSPATH' ) || exit;
 
-//product edit tab function
-function combo_tab( $combo_tabs ) {
-  $combo_tabs['combo_tab'] = 
-    array(
-      'label'   => __( ' Combo Offer ' , 'combo' ),
-      'target'  => 'combo_product_data' ,
-      'class'   => array(),
-      'priority'=> 70
-    );
-  return $combo_tabs;
-}
+include 'vendor/autoload.php';
 
-//Save meta post
-function combo_save_post() {
-  global $post;
-  $post_id = $post->ID;
-  $combo_ids = $_POST['combo_ids'];
-  update_post_meta( $post_id , "combo_ids" , $combo_ids );
-}
-
-//Add to cart
-function combo_add_to_cart() { 
-  $product_ids=get_post_meta( $_POST["product_id"] , "combo_ids" , true );
-  
-  foreach($product_ids as $product_id){
-    WC()->cart->add_to_cart( $product_id); 
-  }
-  $url = get_permalink( $_POST["product_id"] );
-  if (wp_safe_redirect( $url )){
-    exit;
-  }
-}
-
-//secure add to cart
-function combo_product_add_to_cart ()
-{
-  if(!is_admin()){
-    if(isset($_POST['combo_add_to_cart'])) {
-      combo_add_to_cart();
-    } 
-  }      
-}
-
-//file paths
-include plugin_dir_path(__FILE__) . '/app/views/frontend/view-page.php';
-include plugin_dir_path(__FILE__) . '/app/controller/frontend/edit-page.php';
-include plugin_dir_path(__FILE__) . '/app/Route.php';
+Combo\App\Route::hooks();
 
 ?>
